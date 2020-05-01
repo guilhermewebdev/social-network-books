141ca28590b733a1dd25c52fd46fa9e08c7849bb
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-04-30 21:17:50 -0300

    correção de bug no modelo do usuário

--- server/api/accounts/models.py
+++ server/api/accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User as Usr
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
 
-class User(Usr):
+class User(AbstractUser):
     following = models.ManyToManyField(
         'accounts.User',
         related_name='followers',
