30ed4ef8f8b5283ac02bc38e273001eca82dd91d
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-05-02 12:20:44 -0300

    Corrigido bug no mytestcase

--- server/api/api/testcase.py
+++ server/api/api/testcase.py
@@ -5,7 +5,7 @@ from promise import Promise, is_thenable
 class MyClient(JSONWebTokenClient):
 
     def __init__(self, format_error=None, **defaults):
-        self.format_error = format_error | default_format_error
+        self.format_error = format_error or default_format_error
         super().__init__(**defaults)
 
     def format_result(self, result):
@@ -15,7 +15,7 @@ class MyClient(JSONWebTokenClient):
         executed = super().execute(query, variables=variables, **extra)
         if is_thenable(executed):
             return Promise.resolve(executed).then(self.format_result)
-        return self.format_result(executed, self.format_error)
+        return self.format_result(executed)
 
 class MyTestCase(JSONWebTokenTestCase):
     client_class = MyClient
